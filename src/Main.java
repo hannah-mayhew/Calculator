cb96135deac5c2ff1c6987555bacd0addbe5697f
Nathan Brian Gilbertson <Nathanbrian@nathans-air.mynet>
2019-02-12 19:48:30 +0000

    Allowed for number input

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,34 @@
-import java.util.Random;
 import java.util.Scanner;
-
 
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Welcome to the Calculator!");
+        System.out.println("Welcome to the Calculator!");
 
-	Random random = new Random();
-	int first = random.nextInt(100);
-	int second = random.nextInt(100);
+        Scanner scanner = new Scanner(System.in);
 
-	System.out.println("Your first number is: " + first);
-    System.out.println("Your second number is: " + second);
+        System.out.println("Enter your first number");
+        int first = scanner.nextInt();
 
-    System.out.println("Please choose an operation: +/*/-");
-    Scanner scanner = new Scanner(System.in);
-    String input = scanner.next();
-    String add = "+";
-    String subtract = "-";
-    String multiply = "*";
-    int answer = 0;
+        System.out.println("Choose an operation: +/*/-");
+        String input = scanner.next();
+        String add = "+";
+        String subtract = "-";
+        String multiply = "*";
+        int answer = 0;
 
+        System.out.println("Enter your second number");
+        int second = scanner.nextInt();
 
         if (input.equals(add)) {
             answer = first + second;
-    }
+        }
         else if (input.equals(multiply)) {
             answer = first * second;
-    }
+        }
         else if (input.equals(subtract)) {
             answer = first - second;
-    }
+        }
 
         System.out.println("The answer is: " + answer);
     }
